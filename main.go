aa3ff8a2d386cbf138720b2f6c120c3ea190102d
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2022-09-11 03:21:30 +0800

    Define skeleton handlers for snippet creation and deletion. Remove catch-all behavior for the home handler.

--- main.go
+++ main.go
@@ -12,13 +12,31 @@ var (
 
 // Define a home handler function which writes a welcome message to the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	// Disable the catch-all behavior.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	fmt.Fprintf(w, "hello from snippetbox")
+}
+
+// Handles showing of a snippet.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Display a specific snippet")
+}
+
+// Handles creation of a snippet.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create a new snippet")
 }
 
 func main() {
 	// Create a new servemux and apply handler mappings.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// Start a server on the given port and logging any potential error.
 	log.Printf("Starting server on %s", serverPort)
