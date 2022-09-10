3e3e7d6d0344f3fc7fc0a492a7bebc871e00e919
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2022-09-11 04:01:23 +0800

    Setup a file server for static assets

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -16,8 +16,11 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Serve static assets.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// Start a server on the given port and logging any potential error.
 	log.Printf("Starting server on %s", serverPort)
-	err := http.ListenAndServe(serverPort, mux)
-	log.Fatal(err)
+	log.Fatal(http.ListenAndServe(serverPort, mux))
 }
