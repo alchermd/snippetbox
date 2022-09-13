df08194c5645725d2843977f1b6098c4c6d87ab2
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2022-09-14 07:32:04 +0800

    Initial MySQL setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // This struct acts as a container for the shared dependencies of the application.
@@ -16,11 +19,18 @@ type application struct {
 func main() {
 	// Load CLI options
 	serverPort := flag.String("addr", ":4000", "Port that the server runs on")
+	dsn := flag.String("dsn", "web:p@ssword!@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// Setup custom loggers.
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	// Setup dependency injection via struct initialization.
 	app := &application{
@@ -38,4 +48,17 @@ func main() {
 	// Start a server on the given port and logging any potential error.
 	infoLog.Printf("Starting server on %s", srv.Addr)
 	errorLog.Fatal(srv.ListenAndServe())
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
