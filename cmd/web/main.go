166d6a4bba0c37867aa2ca4ae6f1ba946f0034b8
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2022-09-13 19:47:24 +0800

    Create custom loggers, create a http.Server struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,17 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// Load CLI options
 	serverPort := flag.String("addr", ":4000", "Port that the server runs on")
 	flag.Parse()
+
+	// Setup custom loggers.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// Create a new servemux and apply handler mappings.
 	mux := http.NewServeMux()
@@ -21,7 +26,14 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// Initialize a server struct to use the custom error logger.
+	srv := &http.Server{
+		Addr:     *serverPort,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	// Start a server on the given port and logging any potential error.
-	log.Printf("Starting server on %s", *serverPort)
-	log.Fatal(http.ListenAndServe(*serverPort, mux))
+	infoLog.Printf("Starting server on %s", srv.Addr)
+	errorLog.Fatal(srv.ListenAndServe())
 }
