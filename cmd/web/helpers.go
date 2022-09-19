2bcaa0dbd1cd72b385ca837f61fa9860b871cdc0
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2022-09-19 21:47:05 +0800

    Handle global data in templateData

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 // Writes a generic server error response while logging debugging information to stderr.
@@ -35,11 +36,21 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 
 	buf := new(bytes.Buffer)
 
-	err := ts.Execute(buf, td)
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
 	buf.WriteTo(w)
+}
+
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+
+	td.CurrentYear = time.Now().Year()
+
+	return td
 }
